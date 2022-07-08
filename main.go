43aba1a88017dc24a9a6fc5196d76394686fe2cf
Tawhid Hannan <t.hannan93@gmail.com>
2022-07-08 23:27:07 +0100

    Add entrypoint for application.
    Describe in comments a possible API for the application.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	// habitual track <slug>
+	// take the time in YYYY/MM/DD format
+	// lookup the habit by name
+	// create an entry in the log to say the habit was done for the day
+
+	// habitual review
+	// find entries from the log by day
+	// find all active habits
+	// for each day, show the habit completion
+
+	// habitual retire <slug>
+	// find a habit
+	// retire it
+
+	// habitual start <name> <slug>
+	// start a habit
+}
